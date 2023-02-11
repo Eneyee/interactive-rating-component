479dc848a59b2e4dfcbc895cc89bb877bcbf0138
Eneyee <122353277+Eneyee@users.noreply.github.com>
2023-02-11 13:12:00 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let ratingValue;
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5");
+const buttonSubmit = document.querySelector("#buttonSubmit");
+
+function rating() {
+    button1.onclick = ratingValue;
+    console.log("Rating is 1/5")
+}
