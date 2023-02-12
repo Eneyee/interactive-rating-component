ddba479002c2eda1ef4e1a5d2903d9889d09d701
Eneyee <122353277+Eneyee@users.noreply.github.com>
2023-02-12 16:15:58 +0200

    Finished version

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
 const mainContainer = document.querySelector(".main-container");
 const tyContainer = document.querySelector(".ty-container");
 const submitButton = document.querySelector(".btn-submit");
+const rating = document.getElementById("rating");
+const points = document.querySelectorAll(".btn");
 
 submitButton.addEventListener("click", () => {
     mainContainer.style.display = "none"
     tyContainer.classList.remove("hidden")
   })
+points.forEach((point) => {
+    point.addEventListener("click", () => {
+        rating.innerHTML = point.innerHTML
+    })
+})
